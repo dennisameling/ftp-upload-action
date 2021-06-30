d859a8d7cd4511d03fbdf31d12c27e078e2a0de5
Dennis Ameling <dennis@dennisameling.com>
2021-06-30 13:44:22 +0200

    Fix GH Actions workflow + tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,11 +5,28 @@ test('returns folder contents', async () => {
   const fileArray = ['test1.txt', 'test2.txt']
 
   for await (const fileObject of getFiles(
-    path.join(__dirname, 'testFolder'),
-    path.resolve('testFolder')
+    path.join(__dirname, '..', 'dummydata', 'testFolder'),
+    path.resolve(__dirname, '..', 'dummydata', 'testFolder')
     // eslint-disable-next-line no-undef
   ) as AsyncIterable<File>) {
-    const validFile = fileArray.includes(fileObject.filename)
-    await expect(validFile).toEqual(true)
+    expect(fileArray).toContain(fileObject.filename)
   }
 })
+
+test('returns folder contents recursively', async () => {
+  const fileArray = [
+    '/test.txt',
+    'sub1/test.txt',
+    'sub1/subsub1/test.txt',
+    'sub2/test.txt'
+  ]
+
+  for await (const fileObject of getFiles(
+    path.join(__dirname, '..', 'dummydata', 'testFolderRecursive'),
+    path.resolve(__dirname, '..', 'dummydata', 'testFolderRecursive')
+    // eslint-disable-next-line no-undef
+  ) as AsyncIterable<File>) {
+    expect(fileArray).toContain(`${fileObject.folder}/${fileObject.filename}`)
+  }
+})
+
