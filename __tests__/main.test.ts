4412b53aebde00bca9598deca2bd6b5ffd4f023b
Dennis Ameling <dennis@dennisameling.com>
2021-06-30 13:32:21 +0200

    First working version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,28 +1,15 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
 import * as path from 'path'
+import {File, getFiles} from '../src/util'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+test('returns folder contents', async () => {
+  const fileArray = ['test1.txt', 'test2.txt']
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
+  for await (const fileObject of getFiles(
+    path.join(__dirname, 'testFolder'),
+    path.resolve('testFolder')
+    // eslint-disable-next-line no-undef
+  ) as AsyncIterable<File>) {
+    const validFile = fileArray.includes(fileObject.filename)
+    await expect(validFile).toEqual(true)
   }
-  console.log(cp.execFileSync(np, [ip], options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,91 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as ftp from 'basic-ftp'
+import * as path from 'path'
+import {File, getFiles} from './util'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const server: string = core.getInput('server', {required: true})
+    const username: string = core.getInput('username', {required: true})
+    const password: string = core.getInput('password', {required: true})
+    let port = Number(core.getInput('port'))
+    const secureInput: string = core.getInput('secure')
+    let localDir: string = core.getInput('local_dir')
+    let serverDir: string = core.getInput('server_dir')
+    const timeoutMs = 30000
+    let secure = true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const client = new ftp.Client(timeoutMs)
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!port) {
+      port = 21
+    }
+
+    if (secureInput === 'false') {
+      secure = false
+    }
+
+    if (localDir.length === 0) {
+      localDir = './'
+    }
+
+    if (serverDir.length === 0) {
+      serverDir = './'
+    }
+
+    try {
+      core.info('Connecting to server...')
+
+      await client.access({
+        host: server,
+        user: username,
+        password,
+        secure,
+        port
+      })
+
+      core.info(
+        `Successfully connected to server. Starting upload from folder ${localDir}`
+      )
+    } catch (err) {
+      core.setFailed(`Error while connecting to the server: ${err}`)
+      return
+    }
+
+    try {
+      const absRoot = path.resolve(localDir)
+
+      core.debug(`Using this root directory: ${absRoot}`)
+
+      for await (const fileObject of getFiles(
+        localDir,
+        absRoot
+        // eslint-disable-next-line no-undef
+      ) as AsyncIterable<File>) {
+        const localCurrentDir = fileObject.folder.replace(localDir, '')
+        core.info(`Uploading ${localCurrentDir}/${fileObject.filename}...`)
+        const serverFolder = `${serverDir}${localCurrentDir}`
+
+        core.debug(JSON.stringify(fileObject))
+
+        await client.ensureDir(serverFolder)
+        await client.uploadFrom(fileObject.fullPath, fileObject.filename)
+
+        // Go back to the original folder
+        if (fileObject.dirLevel > 0) {
+          for (let i = 0; i < fileObject.dirLevel; i++) {
+            core.debug('Going one folder up...')
+            await client.cdup()
+          }
+        }
+      }
+
+      core.info('All done!')
+    } catch (err) {
+      core.setFailed(`Error while transferring one or more files: ${err}`)
+    }
+
+    client.close()
   } catch (error) {
     core.setFailed(error.message)
   }
