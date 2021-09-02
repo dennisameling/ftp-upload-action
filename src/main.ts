eb90aa89f0ed95a312c4272184045cebea4e7dc3
Dennis Ameling (he/him) <dennis@dennisameling.com>
2021-09-02 09:22:27 +0200

    Update dependencies, simpler error handling (#28)

--- src/main.ts
+++ src/main.ts
@@ -4,95 +4,91 @@ import * as path from 'path'
 import {File, getFiles} from './util'
 
 async function run(): Promise<void> {
+  const server: string = core.getInput('server', {required: true})
+  const username: string = core.getInput('username', {required: true})
+  const password: string = core.getInput('password', {required: true})
+  let port = Number(core.getInput('port'))
+  const secureInput: string = core.getInput('secure')
+  let localDir: string = core.getInput('local_dir')
+  let serverDir: string = core.getInput('server_dir')
+  const timeoutMs = 30000
+  let secure = true
+
+  const client = new ftp.Client(timeoutMs)
+
+  if (!port) {
+    port = 21
+  }
+
+  if (secureInput === 'false') {
+    secure = false
+  }
+
+  if (localDir.length === 0) {
+    localDir = './'
+  }
+
+  if (serverDir.length === 0) {
+    serverDir = './'
+  }
+
   try {
-    const server: string = core.getInput('server', {required: true})
-    const username: string = core.getInput('username', {required: true})
-    const password: string = core.getInput('password', {required: true})
-    let port = Number(core.getInput('port'))
-    const secureInput: string = core.getInput('secure')
-    let localDir: string = core.getInput('local_dir')
-    let serverDir: string = core.getInput('server_dir')
-    const timeoutMs = 30000
-    let secure = true
+    core.info('Connecting to server...')
 
-    const client = new ftp.Client(timeoutMs)
-
-    if (!port) {
-      port = 21
+    if (core.isDebug()) {
+      client.ftp.verbose = true
     }
 
-    if (secureInput === 'false') {
-      secure = false
-    }
+    await client.access({
+      host: server,
+      user: username,
+      password,
+      secure,
+      port
+    })
 
-    if (localDir.length === 0) {
-      localDir = './'
-    }
+    core.info(
+      `Successfully connected to server. Starting upload from folder ${localDir}`
+    )
+  } catch (err) {
+    core.setFailed(`Error while connecting to the server: ${err}`)
+    return
+  }
 
-    if (serverDir.length === 0) {
-      serverDir = './'
-    }
+  try {
+    const absRoot = path.resolve(localDir)
 
-    try {
-      core.info('Connecting to server...')
+    core.debug(`Using this root directory: ${absRoot}`)
 
-      if (core.isDebug()) {
-        client.ftp.verbose = true
-      }
+    for await (const fileObject of getFiles(
+      localDir,
+      absRoot
+      // eslint-disable-next-line no-undef
+    ) as AsyncIterable<File>) {
+      const localCurrentDir = fileObject.folder.replace(localDir, '')
+      core.info(`Uploading ${localCurrentDir}/${fileObject.filename}...`)
+      const serverFolder = `${serverDir}${localCurrentDir}`
 
-      await client.access({
-        host: server,
-        user: username,
-        password,
-        secure,
-        port
-      })
+      core.debug(JSON.stringify(fileObject))
 
-      core.info(
-        `Successfully connected to server. Starting upload from folder ${localDir}`
-      )
-    } catch (err) {
-      core.setFailed(`Error while connecting to the server: ${err}`)
-      return
-    }
+      await client.ensureDir(serverFolder)
+      await client.uploadFrom(fileObject.fullPath, fileObject.filename)
 
-    try {
-      const absRoot = path.resolve(localDir)
-
-      core.debug(`Using this root directory: ${absRoot}`)
-
-      for await (const fileObject of getFiles(
-        localDir,
-        absRoot
-        // eslint-disable-next-line no-undef
-      ) as AsyncIterable<File>) {
-        const localCurrentDir = fileObject.folder.replace(localDir, '')
-        core.info(`Uploading ${localCurrentDir}/${fileObject.filename}...`)
-        const serverFolder = `${serverDir}${localCurrentDir}`
-
-        core.debug(JSON.stringify(fileObject))
-
-        await client.ensureDir(serverFolder)
-        await client.uploadFrom(fileObject.fullPath, fileObject.filename)
-
-        // Go back to the original folder
-        if (fileObject.dirLevel > 0) {
-          for (let i = 0; i < fileObject.dirLevel; i++) {
-            core.debug('Going one folder up...')
-            await client.cdup()
-          }
+      // Go back to the original folder
+      if (fileObject.dirLevel > 0) {
+        for (let i = 0; i < fileObject.dirLevel; i++) {
+          core.debug('Going one folder up...')
+          await client.cdup()
         }
       }
-
-      core.info('All done!')
-    } catch (err) {
-      core.setFailed(`Error while transferring one or more files: ${err}`)
     }
 
-    client.close()
-  } catch (error) {
-    core.setFailed(error.message)
+    core.info('All done!')
+  } catch (err) {
+    core.setFailed(`Error while transferring one or more files: ${err}`)
   }
+
+  client.close()
 }
 
 run()
