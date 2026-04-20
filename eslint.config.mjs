c4b6a696f3854f815ab0a64d570c750b3ec8f630
Dennis Ameling <dennis@dennisameling.com>
2026-04-20 10:48:39 +0200

    Upgrade Node to 24, upgrade deps, switch to ESM (#153)
    * Upgrade Node to 24, upgrade deps, switch to ESM
    
    * Make sure PNPM is installed in CI
    
    * NCC to 0.38.1 due to require bug
    
    Ref: https://github.com/vercel/ncc/issues/1312
    
    * Switch from NCC to Rollup

---
+++ eslint.config.mjs
@@ -0,0 +1,73 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import jest from "eslint-plugin-jest";
+import globals from "globals";
+import github from 'eslint-plugin-github'
+
+export default defineConfig([
+    github.getFlatConfigs().recommended,
+    ...github.getFlatConfigs().typescript,
+    globalIgnores(["**/dist/", "**/lib/", "**/node_modules/", "**/jest.config.js"]),
+    {
+        plugins: {
+            jest,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...jest.environments.globals.globals,
+            },
+
+            ecmaVersion: 9,
+            sourceType: "module",
+
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+
+        rules: {
+            "eslint-comments/no-use": "off",
+            "import/no-namespace": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "error",
+
+            "@typescript-eslint/explicit-member-accessibility": ["error", {
+                accessibility: "no-public",
+            }],
+
+            "@typescript-eslint/no-require-imports": "error",
+            "@typescript-eslint/array-type": "error",
+            "@typescript-eslint/await-thenable": "error",
+            "@typescript-eslint/ban-ts-comment": "error",
+            camelcase: "off",
+            "@typescript-eslint/consistent-type-assertions": "error",
+
+            "@typescript-eslint/explicit-function-return-type": ["error", {
+                allowExpressions: true,
+            }],
+
+            "@typescript-eslint/no-array-constructor": "error",
+            "@typescript-eslint/no-empty-object-type": "error",
+            "@typescript-eslint/no-explicit-any": "error",
+            "@typescript-eslint/no-extraneous-class": "error",
+            "@typescript-eslint/no-for-in-array": "error",
+            "@typescript-eslint/no-inferrable-types": "error",
+            "@typescript-eslint/no-misused-new": "error",
+            "@typescript-eslint/no-namespace": "error",
+            "@typescript-eslint/no-non-null-assertion": "warn",
+            "@typescript-eslint/no-unnecessary-qualifier": "error",
+            "@typescript-eslint/no-unnecessary-type-assertion": "error",
+            "@typescript-eslint/no-useless-constructor": "error",
+            "@typescript-eslint/prefer-for-of": "warn",
+            "@typescript-eslint/prefer-function-type": "warn",
+            "@typescript-eslint/prefer-includes": "error",
+            "@typescript-eslint/prefer-string-starts-ends-with": "error",
+            "@typescript-eslint/promise-function-async": "error",
+            "@typescript-eslint/require-array-sort-compare": "error",
+            "@typescript-eslint/restrict-plus-operands": "error",
+            "@typescript-eslint/unbound-method": "error",
+            "i18n-text/no-en": "off",
+        },
+    },
+]);
